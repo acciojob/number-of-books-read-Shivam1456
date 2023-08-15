f42553852f3aa910c37eefa37878d72704198fbe
Shivam1456 <127660326+Shivam1456@users.noreply.github.com>
2023-08-15 17:11:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,60 @@
-const library = [
+// const library = [
+//   {
+//     author: "Bill Gates",
+//     title: "The Road Ahead",
+//     readingStatus: true,
+//   },
+//   {
+//     author: "Steve Jobs",
+//     title: "Walter Isaacson",
+//     readingStatus: true,
+//   },
+//   {
+//     author: "Suzanne Collins",
+//     title: "Mockingjay: The Final Book of The Hunger Games",
+//     readingStatus: false,
+//   },
+// ];
+
+// const numberOfBooksRead = () => {
+//   // write your code here
+// };
+
+
+// function numberOfBooksRead(library) {
+//   let count = 0;
+  
+//   for (let i = 0; i < library.length; i++) {
+//     if (library[i].readingStatus === true) {
+//       count++;
+//     }
+//   }
+  
+//   return count;
+// }
+
+// Example library object with book data
+let library = [
   {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
+    author: "J.K. Rowling",
+    title: "Harry Potter and the Sorcerer's Stone",
+    readingStatus: true
   },
   {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
+    author: "George Orwell",
+    title: "1984",
+    readingStatus: false
   },
   {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
+    author: "Harper Lee",
+    title: "To Kill a Mockingbird",
+    readingStatus: true
+  }
 ];
 
-const numberOfBooksRead = () => {
-  // write your code here
-};
+let numberOfReadBooks = numberOfBooksRead(library);
+console.log("Number of read books:", numberOfReadBooks); // Logs: Number of read books: 2
+
 
 // Do not change the code below
 

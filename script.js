e723ee33f92c4b690e063cfd923100439c5619b0
Shivam1456 <127660326+Shivam1456@users.noreply.github.com>
2023-08-15 17:12:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,17 +21,17 @@
 // };
 
 
-// function numberOfBooksRead(library) {
-//   let count = 0;
+function numberOfBooksRead(library) {
+  let count = 0;
   
-//   for (let i = 0; i < library.length; i++) {
-//     if (library[i].readingStatus === true) {
-//       count++;
-//     }
-//   }
+  for (let i = 0; i < library.length; i++) {
+    if (library[i].readingStatus === true) {
+      count++;
+    }
+  }
   
-//   return count;
-// }
+  return count;
+}
 
 // Example library object with book data
 let library = [
